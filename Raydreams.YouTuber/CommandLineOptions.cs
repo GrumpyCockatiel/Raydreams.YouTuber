3efe6d4d2b031f0e5fe5ea49bca71eca88186615
Grumpy Cockatiel <tguillory@gmail.com>
2022-03-13 12:06:06 -0500

    Added the CL parser

---
+++ Raydreams.YouTuber/CommandLineOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CommandLine;
+
+namespace Raydreams.YouTuber
+{
+    /// <summary>Options passed to the command line</summary>
+    public class CommandLineOptions
+    {
+        /// <summary></summary>
+        [Option( 'b', "base", Required = false, HelpText = "The base output filename to use" )]
+        public string BaseFilename { get; set; }
+
+        /// <summary></summary>
+        [Option( 'i', "input", Required = false, HelpText = "Filename of the input file with a single column list of YouTube IDs" )]
+        public string InputFile { get; set; }
+
+        /// <summary></summary>
+        [Option( 's', "seq", Required = false, HelpText = "Number sequence start suffixed to the output filename e.g. myvideo-#." )]
+        public int SequenceStart { get; set; }
+    }
+}
--- Raydreams.YouTuber/Program.cs
+++ Raydreams.YouTuber/Program.cs
@@ -4,10 +4,12 @@ using System.IO;
 using System.Linq;
 using Aspose.Html.DataScraping.MultimediaScraping;
 using Aspose.Html.DataScraping.MultimediaScraping.YouTube;
+using CommandLine;
+using Raydreams.YouTuber;
 
 namespace Raydreams.YouTube
 {
-    /// <summary></summary>
+    /// <summary>Simple CL console app to download a collection of YouTube videos specified in the input file</summary>
     public class YouTuber
     {
         #region [ Fields ]
@@ -21,23 +23,42 @@ namespace Raydreams.YouTube
         /// <summary>Folder on the desktop were all the app data is stored</summary>
         public const string AppFolder = "YouTuber";
 
-        /// <summary>Input data file with one video URL per line</summary>
-        public const string InputList = "myvideos.txt";
-
-        /// <summary>The base file name for each video</summary>
-        public const string BaseFilename = "dungeonsdragons";
-
         #endregion [ Fields ]
 
         static void Main( string[] args )
         {
-            YouTuber app = new YouTuber();
+            // get command line options
+            CommandLineOptions options = new CommandLineOptions();
+            var op = Parser.Default.ParseArguments<CommandLineOptions>( args ).WithParsed( o => options = o );
+
+            YouTuber app = new YouTuber( options );
             app.Run();
 
             Console.WriteLine( "Done" );
         }
 
+        /// <summary>Constructor</summary>
+        /// <param name="options"></param>
+        public YouTuber(CommandLineOptions options)
+        {
+            if (options != null)
+            {
+                this.InputList = options.InputFile;
+                this.BaseFilename = options.BaseFilename;
+                this.SequenceStart = options.SequenceStart;
+            }
+        }
+
         #region [ Properties ]
+
+        /// <summary>Input data file with one video URL per line</summary>
+        public string InputList { get; set; } = "myvideos.txt";
+
+        /// <summary>The base file name for each video</summary>
+        public string BaseFilename { get; set; } = "video";
+
+        /// <summary>The integer to start counting from</summary>
+        public int SequenceStart { get; set; } = 1;
 
         /// <summary>List of Video IDs</summary>
         protected List<String> Videos { get; set; }
@@ -51,7 +72,6 @@ namespace Raydreams.YouTube
         {
             try
             {
-
                 // need to check all the folders exist
 
                 // load the input data
@@ -67,7 +87,7 @@ namespace Raydreams.YouTube
                 {
                     try
                     {
-                        this.DownloadVideo( url, $"{BaseFilename}-{count + 1}" );
+                        this.DownloadVideo( url, $"{BaseFilename}-{this.SequenceStart + count}" );
                     }
                     catch
                     {
@@ -91,7 +111,21 @@ namespace Raydreams.YouTube
             if ( !File.Exists( path ) )
                 return;
 
-            this.Videos = File.ReadAllLines(path).ToList();
+            // init the list to load
+            this.Videos = new List<string>();
+
+            // read everything
+            var lines = File.ReadAllLines( path ).ToList();
+
+            // parse all the input
+            // later extract just the ID from a complete URL
+            foreach (string line in lines)
+            {
+                if ( String.IsNullOrWhiteSpace( line ) )
+                    continue;
+
+                this.Videos.Add( line.Trim() );
+            }
         }
 
         /// <summary>Actually DL the video sync for now</summary>
@@ -99,10 +133,6 @@ namespace Raydreams.YouTube
         /// <param name="filename">Name of the output file</param>
         public void DownloadVideo(string vid, string filename)
         {
-            // Expected video properties, for example, "Title" and "Duration" 
-            //string expectedTitle = "HAPPY - Walk off the Earth Ft. Parachute";
-            //int expectedDuration = 60 * 3 + 55;
-
             var url = new Uri( $"https://www.youtube.com/watch?v={vid}" );
 
             if ( String.IsNullOrWhiteSpace(filename) || !url.IsAbsoluteUri )
@@ -120,27 +150,26 @@ namespace Raydreams.YouTube
             // Cast a videoInfo to YouTubeVideoInfo type
             var youTubeVideoInfo = videoInfo as YouTubeVideoInfo;
 
-            if ( youTubeVideoInfo != null )
-            {
-                // Get the first element from the formats collection with minimal bitrate and present audio and video codecs
-                var format = youTubeVideoInfo.Formats.OrderBy( f => f.Bitrate ).First( f => f.AudioCodec != null && f.VideoCodec != null );
+            // log nothing
+            if ( youTubeVideoInfo == null )
+                return;
+            
+            // Get the first element from the formats collection with minimal bitrate and present audio and video codecs
+            var format = youTubeVideoInfo.Formats.OrderBy( f => f.Bitrate ).First( f => f.AudioCodec != null && f.VideoCodec != null );
 
-                // Get the extension for the output file
-                var ext = string.IsNullOrEmpty( format.Extension ) ? "mp4" : format.Extension;
+            // Get the extension for the output file
+            var ext = String.IsNullOrEmpty( format.Extension ) ? "mp4" : format.Extension;
 
-                // Get the full file path for the output file
-                var filePath = Path.Combine( DesktopPath, filename + "." + ext );
+            // Get the full file path for the output file
+            var filePath = Path.Combine( DesktopPath, AppFolder, DownloadFolder, $"{filename}.{ext}" );
 
-                // Download YouTube video
-                multimedia.Download( format, filePath );
+            // Download YouTube video
+            multimedia.Download( format, filePath );
 
-                // Show YouTube video info
-                Console.WriteLine( "Title: {0}", youTubeVideoInfo.Title );
-                Console.WriteLine( "Description: {0}", youTubeVideoInfo.Description );
-                Console.WriteLine( "Duration: {0}", youTubeVideoInfo.Duration );
-                Console.WriteLine( "Thumbnails count: {0}", youTubeVideoInfo.Thumbnails.Count );
-                Console.WriteLine( "Formats count: {0}", youTubeVideoInfo.Formats.Count );
-            }
+            // create a log statement for later
+            string log = $"Title:{youTubeVideoInfo.Title}; Description:{youTubeVideoInfo.Description}; Duration:{youTubeVideoInfo.Duration}; Thumbnails Count:{youTubeVideoInfo.Thumbnails.Count}; Formats count: {0}, { youTubeVideoInfo.Formats.Count} ";
+
+            // later return the info
         }
 
         #endregion [ Methods ]
